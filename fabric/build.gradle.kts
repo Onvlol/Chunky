0fd5e402c8552456d7a91a984b2039ad34eb6f39
Onvlol <IzeAack@outlook.com>
2024-08-23 11:07:17 -0400

    Update build.gradle.kts

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     mappings(group = "net.fabricmc", name = "yarn", version = "1.21+build.2", classifier = "v2")
     modImplementation(group = "net.fabricmc", name = "fabric-loader", version = "0.16.2")
     modImplementation(group = "net.fabricmc.fabric-api", name = "fabric-api", version = "0.102.0+1.21")
-    modCompileOnly(group = "me.lucko", name = "fabric-permissions-api", version = "0.3.1-SNAPSHOT")
+    modCompileOnly(group = "me.lucko", name = "fabric-permissions-api", version = "0.3.1")
     implementation(project(":chunky-common"))
     shade(project(":chunky-common"))
 }
